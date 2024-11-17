739e721dcb1031e95981910a72a0e7cbd04e17c2
Nikizh <nikizh007@gmail.com>
2024-11-17 12:59:41 +0530

    adding completed button

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -4,10 +4,17 @@ import "../App.css";
 const Todo = () => {
   const [Task, setTask] = useState(Array().fill(null));
   const [NewTask, setNewTask] = useState("");
+
   function handleTasks() {
-    let tasks = [...Task, NewTask];
+    let tasks = [...Task, {text: NewTask , completed : false}];
     setTask(tasks);
     setNewTask("");
+  }
+
+  function completeTasks(id) {
+    let updated = [...Task];
+    updated[id].completed = !updated[id].completed
+    setTask(updated)
   }
 
   function handleNewTasks(event) {
@@ -28,7 +35,7 @@ const Todo = () => {
 
   return (
     <>
-      <div className="container border-black border-2px mx-auto bg-white py-[50px] my-[50px] w-[40%] rounded-[10px] shadow-lg">
+      <div className="container mx-auto bg-white py-[50px] my-[50px] w-[40%] rounded-[10px] shadow-lg">
         <h1 className="title text-[30px] my-[5px]">ToDo List</h1>
         <input
           type="text"
@@ -39,19 +46,22 @@ const Todo = () => {
         />
         <button
           onClick={handleTasks}
-          className="bg-blue-500bg-blue-500 hover:bg-green-600 text-gray-400 font-bold py-2 px-4 rounded shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          className="hover:bg-green-600 text-gray-400 font-bold py-2 px-4 rounded shadow-md hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
         >
           Submit
         </button>
-        <tr className="list-none" id="liist">
+        <ul className="list-none" id="liist">
           {Task.map((task ,index) => (
-            <div className="mx-[30px] border-[2px] border-gray w-[550px] my-[10px] px-[7px] py-[10px] flex justify-between items-center rounded-[5px]
+            <div className="mx-[30px] border-[2px] border-gray w-[550px] my-[10px] px-[7px] py-[10px] flex justify-between rounded-[5px]
             ">
-            <li className="">{task}</li>
+             <li className={`flex-1 ${task.completed ? "line-through" : ""}`}> {task.text} </li>
+            <div className="btns flex gap-2">
+            <button className="completed-btn bg-green-400 px-[10px] py-[5px] rounded-[5px]" key={index} onClick={() => {completeTasks(index)}}>&#10004;</button>
             <button className="delete-btn bg-red-400 px-[10px] py-[5px] rounded-[5px]" onClick={() => {handleDelete(index)}}>&#128465;</button>
             </div>
+            </div>
           ))}
-        </tr>
+        </ul>
       </div>
     </>
   );
